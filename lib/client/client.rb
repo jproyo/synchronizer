71afa6fc59346938468926f136bf2516b5055110
jproyo <juanpablo.royo@gmail.com>
2012-01-03 14:38:07 -0300

    Documentation of ClientConnection Class

--- lib/client/client.rb
+++ lib/client/client.rb
@@ -6,6 +6,8 @@ require "#{path}/connector"
 
 $LOG = Logger.new('client_log.log','daily')
 
+# ClientConnection class is the first entry point to send data to a syncronizer server.
+# Instantiate this class and pass as an argument the host and port server you want to connect to.
 class ClientConnection
   
   def initialize(host, port)
@@ -13,17 +15,35 @@ class ClientConnection
     @port = port
   end
 
-  def connect(data)
-    @handler = EventMachine::connect @host, @port, Sender, data, @host, @port
-  end
-
+  # Send any data that you want to a server syncronizer.
+  # This method receives a proc too in order to build the byte array as the client wants.
+  # The client has the responsability to build in the block proc an array of byte's array
+  # where the length of the main array is the amount of chunks to be transfer to the server.
+  # Example:
+  #	client = ClientConnection.new('localhost',8080)
+  #	client.send_data ARGV[0] do |data|
+  #  		complete_window = []
+  #  		bytes_msgs = IO.read(data).unpack('C*').to_a
+  #  		bytes_msgs.each_slice(10) do |slice|
+  #     		complete_window << slice
+  #  		end
+  #  		complete_window
+  #	end
+  #
+  # In this example we are reading from a file and building chunks of 10 bytes to be sent
   def send_data(data)
     data_to_send = yield data
     EventMachine::run do
-	connect data_to_send
+        connect data_to_send
     end
   end
-  
+
+
+  private 
+  def connect(data)
+  	EventMachine::connect @host, @port, Sender, data, @host, @port
+  end
+
 end
 
 client = ClientConnection.new('localhost',8080)
@@ -35,4 +55,5 @@ client.send_data ARGV[0] do |data|
     end
     complete_window
 end
+
 
